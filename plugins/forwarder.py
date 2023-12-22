6b2f7f49d75e15571f9ffe071c11c6a91bccae08
Joseph Flinn <joseph.s.flinn@gmail.com>
2023-12-22 13:17:27 -0800

    Building out a forwarder router depending on the type of message: new message or a threaded ping

--- plugins/forwarder.py
+++ plugins/forwarder.py
@@ -12,13 +12,36 @@ class ResponderBot(MachineBasePlugin):
     @listen_to(regex=rf"<@U01NJ9RDQ2D>")
     async def forward_message(self, msg):
         logger.info(msg.text)
-        forwarded_msg = await self.say(
+
+        if msg.in_thread:
+            await self.forward_threaded_message(msg)
+        else:
+            await self.forward_new_message(msg)
+
+    async def forward_threaded_message(self, msg):
+        message_link = await self.web_client.chat_getPermalink(
+            channel=msg.channel.id,
+            message_ts=msg.ts
+        )
+        forwarded_message = await self.say(
+            channel=self.settings['FORWARDER_INBOX'],
+            text=f"Thread mention: {message_link['permalink']}"
+        )
+        forwarded_link = await self.web_client.chat_getPermalink(
+            channel=forwarded_message['channel'],
+            message_ts=forwarded_message['ts']
+        )
+
+        await msg.say(text=f"{forwarded_link['permalink']}", thread_ts=msg.ts)
+
+    async def forward_new_message(self, msg):
+        forwarded_message = await self.say(
             channel=self.settings['FORWARDER_INBOX'],
             text=msg.text
         )
         link = await self.web_client.chat_getPermalink(
-            channel=forwarded_msg['channel'],
-            message_ts=forwarded_msg['ts']
+            channel=forwarded_message['channel'],
+            message_ts=forwarded_message['ts']
         )
 
-        await msg.say(text=f"{link['permalink']}", thread_ts=msg.ts)
+        await msg.say(text=f"This message has been sent to {self.settings['FORWARDER_INBOX']}. Please visit the link for further coordination: {link['permalink']}", thread_ts=msg.ts)
